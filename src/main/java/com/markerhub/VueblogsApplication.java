eaf055ea620bb2df4ded2b4fba8f0098f049654a
Harold <huang_ziyang@yltsoft.com>
2020-07-12 22:35:39 +0800

    后端vueblog

---
+++ src/main/java/com/markerhub/VueblogsApplication.java
@@ -0,0 +1,13 @@
+package com.markerhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueblogsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueblogsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/markerhub/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.markerhub.controller;
+
+import com.markerhub.common.lang.Result;
+import com.markerhub.entity.User;
+import com.markerhub.service.UserService;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @RequiresAuthentication
+    @GetMapping("/index")
+    public Result index() {
+        User user = userService.getById(1L);
+        return Result.succ(user);
+    }
+
+    @PostMapping("/save")
+    public Result save(@Validated @RequestBody User user) {
+
+
+
+
+        return Result.succ(user);
+    }
+
+}
